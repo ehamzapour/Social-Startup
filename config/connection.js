15dad24526ae464a80bf9a2ce67e3bd76f33ee43
Elham Hamzapour <ehamzapour@gmail.com>
2023-01-18 13:31:39 -0600

    still having error with mongoose

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
 connect(connectionString, {
     useNewUrlParser: true,

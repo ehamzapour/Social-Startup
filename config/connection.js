ea6497b643d84827284fe66e8222e0ec53e5fd40
Elham Hamzapour <ehamzapour@gmail.com>
2023-01-18 20:33:31 -0600

    submitting will work on later

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
 const { connect, connection } = require('mongoose');
+require('dotenv').config();
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
+
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hygsx7y.mongodb.net/socialDB';
 
 connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
 
 module.exports = connection;
